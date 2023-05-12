e81a6402d15764be22f048ba15ef36de12ee5478
HernandoT <hernandotandra02@gmail.com>
2023-05-12 16:51:08 +0700

    Add google testimonials

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,7 +1,21 @@
 import "./Testimonials.css";
 import { FaQuoteLeft } from "react-icons/fa";
+import { useEffect } from "react";
 
 const Testimonials = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://apps.elfsight.com/p/platform.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+  
   return (
     <div className="testimonials">
       <div className="testimonialsTitle">
@@ -9,7 +23,7 @@ const Testimonials = () => {
         <p>Customer Testimonials</p>
       </div>
       <div className="testimonialsContent">
-        {Array.from({ length: 3 }).map((testimonial, index) => (
+        {/* {Array.from({ length: 3 }).map((testimonial, index) => (
           <div className="testimonial" key={index}>
             <FaQuoteLeft className="testimonialIcon"/>
             <div className="testimonialText">
@@ -18,7 +32,8 @@ const Testimonials = () => {
             </div>
             <div className="testimonialName">Name - Lorem ipsum</div>
           </div>
-        ))}
+        ))} */}
+        <div class="elfsight-app-9a5e0f8d-c007-4fb8-abc5-895f8e6331a0"></div>
       </div>
     </div>
   );
