cf541acc947e57c0ae9656563c3a0d427e74f2f3
HernandoT <hernandotandra02@gmail.com>
2023-05-20 11:41:59 +0700

    Add search bar collection page

--- src/pages/Collections/Collections.js
+++ src/pages/Collections/Collections.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import AdminTitle from "../../components/AdminTitle/AdminTitle";
 import "./Collections.css";
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Collections = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="collections">
-      <AdminTitle props={"All Collections"}/>
+      <AdminTitle props={"All Collections"} />
+      <div className="collections-content">
+        <div className="collections-search">
+          <TextField
+            id="search"
+            type="search"
+            label="Cari menurut nama"
+            className="collections-search-input"
+            value={searchTerm}
+            onChange={handleChange}
+            sx={{ width: "65%" }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <button className="collections-add">+ TAMBAH BARANG</button>
+        </div>
+      </div>
     </div>
   );
 };
