a9f0e106f1faa3c25ebf578e361316cc68ce7c37
HernandoT <hernandotandra02@gmail.com>
2023-05-22 16:50:20 +0700

    Fix History page

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -6,8 +6,8 @@ import SupportEngine from "../../SupportEngine";
 
 import "./History.css";
 import { useState } from "react";
-import invoice from "../../utils/assets/invoice.svg"
-import appointment from "../../utils/assets/appointment.svg"
+import invoice from "../../utils/assets/invoice.svg";
+import appointment from "../../utils/assets/appointment.svg";
 
 const History = () => {
   const isLoged = localStorage.getItem("isLoged");
@@ -25,20 +25,52 @@ const History = () => {
         <TabContext value={currentTab}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList centered>
-              <Tab label="Invoice" value="1" onClick={handleTabClick} />
-              <Tab label="Appointment" value="2" onClick={handleTabClick} />
+              <Tab
+                label="Invoice"
+                value="1"
+                onClick={handleTabClick}
+                style={{ fontSize: "1.5rem" }}
+              />
+              <Tab
+                label="Appointment"
+                value="2"
+                onClick={handleTabClick}
+                style={{ fontSize: "1.5rem" }}
+              />
             </TabList>
           </Box>
           <TabPanel value="1">
-            <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-              <p style={{fontSize: "2rem"}}>Kamu tidak memiliki Riwayat Invoice</p>
-              <img src={invoice} style={{width: "50vw", height: "50vh"}}></img>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <p style={{ fontSize: "2rem" }}>
+                Kamu tidak memiliki Riwayat Invoice
+              </p>
+              <img
+                src={invoice}
+                style={{ width: "50vw", height: "50vh" }}
+              ></img>
             </div>
           </TabPanel>
           <TabPanel value="2">
-            <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-              <p style={{fontSize: "2rem"}}>Kamu tidak memiliki Riwayat Appointment</p>
-              <img src={appointment} style={{width: "50vw", height: "50vh"}}></img>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <p style={{ fontSize: "2rem" }}>
+                Kamu tidak memiliki Riwayat Appointment
+              </p>
+              <img
+                src={appointment}
+                style={{ width: "50vw", height: "50vh" }}
+              ></img>
             </div>
           </TabPanel>
         </TabContext>
