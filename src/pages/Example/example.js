0d89a770484e7ef976c55752a0e8d7725c44d887
HernandoT <scretz77@gmail.com>
2023-05-23 22:58:54 +0700

    feat: example page

---
+++ src/pages/Example/example.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+const Example = () => {
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+
+  const onSubmit = React.useCallback(
+    (e) => async () => {
+      try {
+        e.preventDefault();
+        const data = {
+          name,
+          email,
+          phone,
+          password: 123456,
+        };
+
+        console.log(data);
+      } catch {
+      } finally {
+      }
+    },
+    [email, name, phone]
+  );
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Example;
