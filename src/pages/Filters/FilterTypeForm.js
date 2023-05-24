be052d6b4e1f75199d2211844f0dce663c90ed39
HernandoT <hernandotandra02@gmail.com>
2023-05-24 22:01:23 +0700

    feat: category form, type form

---
+++ src/pages/Filters/FilterTypeForm.js
@@ -0,0 +1,92 @@
+import { Flex, Paper, Text } from "@mantine/core";
+import { Button, Modal, TextField } from "@mui/material";
+import * as React from "react";
+import Separator from "../../components/separator";
+import { notifications } from "@mantine/notifications";
+
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../services/firebase";
+
+const FilterTypeForm = ({
+  data = { nama: "" },
+  open = true,
+  onClose,
+}) => {
+  const [nama, setNama] = React.useState(data.nama);
+
+  const insertType = async (nama) => {
+    try {
+      await addDoc(collection(db, "jenis"), {
+        nama_jenis: nama,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onSubmit = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      try {
+        insertType(nama);
+        notifications.show({
+          title: "Tambah Jenis",
+          message: "Jenis baru telah berhasil ditambahkan",
+          color: "teal",
+        });
+        onClose();
+      } catch {
+        notifications.show({
+          title: "Tambah Jenis",
+          message: "Jenis baru gagal ditambahkan",
+          color: "red",
+        });
+      } finally {
+      }
+    },
+    [nama]
+  );
+
+  return (
+    <Modal open={open}>
+      <Paper
+        p={36}
+        miw={400}
+        style={{
+          transform: "translate(-50%, -50%)",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+        }}
+      >
+        <form onSubmit={onSubmit}>
+          <Flex direction="column">
+            <Text fz={20} fw={600}>
+              Tambah Jenis
+            </Text>
+            <Separator _gap={24} />
+
+            <TextField
+              value={nama}
+              onChange={(e) => setNama(e.target.value)}
+              label="Nama Jenis"
+              required
+            />
+            <Separator _gap={24} />
+
+            <Flex justify="flex-end">
+              <Button variant="text" color="error" onClick={onClose}>
+                Batal
+              </Button>
+              <Button variant="text" type="submit">
+                Simpan
+              </Button>
+            </Flex>
+          </Flex>
+        </form>
+      </Paper>
+    </Modal>
+  );
+};
+
+export default FilterTypeForm;
--- src/pages/Filters/Filters.js
+++ src/pages/Filters/Filters.js
@@ -4,13 +4,19 @@ import { Chip, Stack } from "@mui/material";
 import * as React from "react";
 import { useDisclosure } from "@mantine/hooks";
 import FilterColorForm from "./FilterColorForm";
+import FilterTypeForm from "./FilterTypeForm";
+import FilterCategoryForm from "./FilterCategoryForm";
 
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../services/firebase";
 
 const Filters = () => {
   const [colors, setColors] = React.useState([]);
-  const [opened, { open, close }] = useDisclosure(false);
+  const [categories, setCategories] = React.useState([]);
+  const [types, setTypes] = React.useState([]);
+  const [openedColor, { open: openColor, close: closeColor }] = useDisclosure(false);
+  const [openedCategory, { open: openCategory, close: closeCategory }] = useDisclosure(false);
+  const [openedType, { open: openType, close: closeType }] = useDisclosure(false);
 
   const fetchColors = async () => {
     await getDocs(collection(db, "warna")).then((querySnapshot) => {
@@ -22,12 +28,31 @@ const Filters = () => {
     });
   };
 
+  const fetchCategories = async () => {
+    await getDocs(collection(db, "kategori")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setCategories(newData);
+    });
+  };
+
+  const fetchTypes = async () => {
+    await getDocs(collection(db, "jenis")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setTypes(newData);
+    });
+  };
+
   React.useEffect(() => {
     fetchColors();
+    fetchCategories();
+    fetchTypes();
   }, []);
-
-  const categories = ["Cheongsam/Changsan", "Party", "Wedding"];
-  const types = ["Pria", "Wanita"];
 
   const handleClick = () => {
     console.info("You clicked the Chip.");
@@ -39,7 +64,7 @@ const Filters = () => {
       <div className="filters-content">
         <div style={{ margin: "20px 0 10px" }}>
           <b>Warna</b>
-          <button style={{ marginLeft: "20px" }} onClick={open}>
+          <button style={{ marginLeft: "20px", width: "40px" }} onClick={openColor}>
             +
           </button>
         </div>
@@ -58,12 +83,13 @@ const Filters = () => {
         </Stack>
         <div style={{ margin: "20px 0 10px" }}>
           <b>Kategori</b>
+          <button style={{ marginLeft: "20px", width: "40px" }} onClick={openCategory}>+</button>
         </div>
         <Stack direction="row" flexWrap="wrap">
           {categories.map((category) => {
             return (
               <Chip
-                label={category}
+                label={category.nama_kategori}
                 variant="outlined"
                 onClick={handleClick}
                 className="filters-chip"
@@ -73,12 +99,13 @@ const Filters = () => {
         </Stack>
         <div style={{ margin: "20px 0 10px" }}>
           <b>Jenis</b>
+          <button style={{ marginLeft: "20px", width: "40px" }} onClick={openType}>+</button>
         </div>
         <Stack direction="row" flexWrap="wrap">
           {types.map((type) => {
             return (
               <Chip
-                label={type}
+                label={type.nama_jenis}
                 variant="outlined"
                 onClick={handleClick}
                 className="filters-chip"
@@ -87,7 +114,9 @@ const Filters = () => {
           })}
         </Stack>
       </div>
-      <FilterColorForm onClose={close} open={opened} />
+      <FilterColorForm onClose={closeColor} open={openedColor} />
+      <FilterCategoryForm onClose={closeCategory} open={openedCategory} />
+      <FilterTypeForm onClose={closeType} open={openedType} />
     </div>
   );
 };
