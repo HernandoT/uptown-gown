8a00b1241ab7c6573db1aab9c05c191dfea71ab4
HernandoT <hernandotandra02@gmail.com>
2023-05-31 16:57:27 +0700

    fix: add appointment

--- src/pages/Appointments/AppointmentForm.js
+++ src/pages/Appointments/AppointmentForm.js
@@ -6,6 +6,7 @@ import { Flex, Modal } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import CustomerForm from "../Customer/CustomerForm";
 import Separator from "../../components/separator";
+import { notifications } from "@mantine/notifications";
 
 import * as Yup from "yup";
 import useYupValidationResolver from "../../hooks/use-yup-resolver";
@@ -17,8 +18,12 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import TextInputField from "../../components/field/text-input";
+import { Timestamp } from "firebase/firestore";
+import dayjs from "dayjs";
+import { createAppointment } from "../../services/appointment";
 
 const AppointmentForm = () => {
+  const [customer, setCustomer] = React.useState();
   const [opened, { open, close }] = useDisclosure(false);
 
   const navigate = useNavigate();
@@ -31,6 +36,7 @@ const AppointmentForm = () => {
   const yupSchema = React.useMemo(
     () =>
       Yup.object().shape({
+        customer: Yup.string().required("Harap pilih Customer terlebih dahulu"),
         tanggal: Yup.date().required("Harap pilih tanggal"),
       }),
     []
@@ -46,7 +52,22 @@ const AppointmentForm = () => {
 
   const onSubmit = React.useCallback(async (values) => {
     try {
+      createAppointment({
+        keterangan: values.keterangan,
+        id_customer: values.customer,
+        tanggal: Timestamp.fromDate(new Date(values.tanggal)),
+      });
+      notifications.show({
+        title: "Tambah Appointment",
+        message: "Pengeluaran baru telah berhasil ditambahkan",
+        color: "teal",
+      });
     } catch (e) {
+      notifications.show({
+        title: "Tambah Appointment",
+        message: "Appointment baru gagal ditambahkan",
+        color: "red",
+      });
     } finally {
     }
   }, []);
@@ -58,7 +79,7 @@ const AppointmentForm = () => {
   return (
     <div className="appointment-form">
       <AdminTitle props={"Appointment"} />
-      <div onClick={() => navigate(-1)} style={{cursor: "pointer"}}>
+      <div onClick={() => navigate(-1)} style={{ cursor: "pointer" }}>
         <i
           class="fa fa-chevron-left"
           aria-hidden="true"
@@ -69,7 +90,7 @@ const AppointmentForm = () => {
       <div className="appointment-form-content">
         <Form onSubmit={onSubmit} methods={methods}>
           <div className="appointment-form-customer">
-            <CustomerSelectInput style={{ flex: "70" }} />
+            <CustomerSelectInput style={{ flex: "70" }} name="customer" />
             <button
               className="appointment-form-customer-add"
               onClick={onClickAdd}
@@ -79,7 +100,11 @@ const AppointmentForm = () => {
           </div>
           <Separator _gap={24} />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker label="Tanggal Pengeluaran" format="DD/MM/YYYY" />
+            <DatePicker
+              label="Tanggal Pengeluaran"
+              format="DD/MM/YYYY"
+              value={dayjs(new Date())}
+            />
           </LocalizationProvider>
           <Separator _gap={24} />
           <TextInputField
