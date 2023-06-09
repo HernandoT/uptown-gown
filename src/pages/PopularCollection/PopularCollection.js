1562b504b7d185bfefb3572723fb0f7aba212ad5
HernandoT <scretz77@gmail.com>
2023-06-09 20:30:37 +0700

    fix: collection

--- src/pages/PopularCollection/PopularCollection.js
+++ src/pages/PopularCollection/PopularCollection.js
@@ -1,7 +1,7 @@
 import AdminTitle from "../../components/AdminTitle/AdminTitle";
 import "./PopularCollection.css";
 import Form from "../../components/field/form";
-import { useForm } from "react-hook-form";
+import { useForm, useFormContext, useWatch } from "react-hook-form";
 import * as React from "react";
 import * as Yup from "yup";
 import CollectionSelectInput from "../../components/Select/collection-select-input";
@@ -10,46 +10,71 @@ import useYupValidationResolver from "../../hooks/use-yup-resolver";
 import { getCollections } from "../../services/collection";
 import { useQuery } from "@tanstack/react-query";
 
-const PopularCollection = () => {
-  const [idGambar1, setIdGambar1] = React.useState("");
-  const [idGambar2, setIdGambar2] = React.useState("");
-  const [idGambar3, setIdGambar3] = React.useState("");
-  const [idGambar4, setIdGambar4] = React.useState("");
-  const { data, isFetching } = useQuery(["get-collections"], () =>
-    getCollections()
+const CollectionView = () => {
+  const { control } = useFormContext();
+  const values = useWatch({
+    control,
+    name: ["file1", "file2", "file3", "file4"],
+  });
+  console.log(values);
+  return (
+    <div className="popular-collection-display">
+      {values.map((value, index) =>
+        !!value ? (
+          <img src={value} className={`popular-collection-${index + 1}`} />
+        ) : (
+          <div className={`popular-collection-${index + 1}`}>{index + 1}</div>
+        )
+      )}
+    </div>
   );
+};
 
-  React.useEffect(() => {
-    if (!isFetching) {
-      const collections = data?.data;
-      const gambar1 = collections.filter((collection) => {
-        return collection.popular_collection === 1;
-      });
-      setIdGambar1(gambar1[0].id);
-      const gambar2 = collections.filter((collection) => {
-        return collection.popular_collection === 2;
-      });
-      setIdGambar2(gambar2[0].id);
-      const gambar3 = collections.filter((collection) => {
-        return collection.popular_collection === 3;
-      });
-      setIdGambar3(gambar3[0].id);
-      const gambar4 = collections.filter((collection) => {
-        return collection.popular_collection === 4;
-      });
-      setIdGambar4(gambar4[0].id);
-      console.log(idGambar1, idGambar2, idGambar3, idGambar4)
-    }
-  }, [isFetching, data?.data]);
+const PopularCollection = () => {
+  // const [idGambar1, setIdGambar1] = React.useState("");
+  // const [idGambar2, setIdGambar2] = React.useState("");
+  // const [idGambar3, setIdGambar3] = React.useState("");
+  // const [idGambar4, setIdGambar4] = React.useState("");
+  // const [initiate, setInitiate] = React.useState(false);
+  // const { data, isFetching } = useQuery(["get-collections-data"], () =>
+  //   getCollections()
+  // );
+
+  // React.useEffect(() => {
+  //   if (!initiate && data?.data) {
+  //     const collections = data?.data;
+  //     const gambar1 = collections.filter((collection) => {
+  //       return collection.popular_collection === 1;
+  //     });
+  //     setIdGambar1(gambar1[0].id);
+  //     const gambar2 = collections.filter((collection) => {
+  //       return collection.popular_collection === 2;
+  //     });
+  //     setIdGambar2(gambar2[0].id);
+  //     const gambar3 = collections.filter((collection) => {
+  //       return collection.popular_collection === 3;
+  //     });
+  //     setIdGambar3(gambar3[0].id);
+  //     const gambar4 = collections.filter((collection) => {
+  //       return collection.popular_collection === 4;
+  //     });
+  //     setIdGambar4(gambar4[0].id);
+  //     setInitiate(true);
+  //   }
+  // }, [data?.data, initiate, idGambar1, idGambar2, idGambar3, idGambar4]);
 
   const defaultValues = React.useMemo(
     () => ({
-      gambar1: idGambar1,
-      gambar2: idGambar2,
-      gambar3: idGambar3,
-      gambar4: idGambar4,
+      gambar1: "",
+      gambar2: "",
+      gambar3: "",
+      gambar4: "",
+      file1: "",
+      file2: "",
+      file3: "",
+      file4: "",
     }),
-    [idGambar1, idGambar2, idGambar3, idGambar4]
+    []
   );
 
   const yupSchema = React.useMemo(
@@ -78,36 +103,35 @@ const PopularCollection = () => {
       console.log(e);
     } finally {
     }
-  });
+  }, []);
 
   return (
     <div className="popular-collection">
-      <AdminTitle props={"Popular Collection"} />
-      <div className="popular-collection-content">
-        <div className="popular-collection-selector">
-          {/* {isFetching ? (
-            <></>
-          ) : ( */}
-            <Form onSubmit={onSubmit} methods={methods}>
-              <CollectionSelectInput label="Gambar 1" name="gambar1" />
-              <Separator _gap={36} />
-              <CollectionSelectInput label="Gambar 2" name="gambar2" />
-              <Separator _gap={36} />
-              <CollectionSelectInput label="Gambar 3" name="gambar3" />
-              <Separator _gap={36} />
-              <CollectionSelectInput label="Gambar 4" name="gambar4" />
-              <Separator _gap={36} />
-              <button className="save-popular-collection">SIMPAN</button>
-            </Form>
-          {/* )} */}
+      <Form onSubmit={onSubmit} methods={methods}>
+        <AdminTitle props={"Popular Collection"} />
+        <div className="popular-collection-content">
+          <div className="popular-collection-selector">
+            {["gambar1", "gambar2", "gambar3", "gambar4"].map(
+              (value, index) => {
+                return (
+                  <>
+                    <CollectionSelectInput
+                      label={`Gambar ${index + 1}`}
+                      name={value}
+                      onAfterChangeDetail={(value) =>
+                        methods.setValue(`file${index + 1}`, value.extra.gambar)
+                      }
+                    />
+                    <Separator _gap={36} />
+                  </>
+                );
+              }
+            )}
+            <button className="save-popular-collection">SIMPAN</button>
+            <CollectionView />
+          </div>
         </div>
-        <div className="popular-collection-display">
-          <div className="popular-collection-1">1</div>
-          <div className="popular-collection-2">2</div>
-          <div className="popular-collection-3">3</div>
-          <div className="popular-collection-4">4</div>
-        </div>
-      </div>
+      </Form>
     </div>
   );
 };
