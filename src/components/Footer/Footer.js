eda1352d09364687f259b5e7d159a1fcd3f8cad5
silvianataa <silvianataa@gmail.com>
2023-07-04 23:07:29 +0700

    fix styling footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 import logo from "../../utils/assets/logo.png";
-import { FaWhatsapp ,FaRegEnvelope ,FaInstagram} from "react-icons/fa";
+import { FaTiktok ,FaRegEnvelope ,FaInstagram} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -9,21 +9,15 @@ const Footer = () => {
         <img src={logo} alt="" />
       </div>
       <div className="footerCompany">
-        <p>
-          <b>Company</b>
-        </p>
-        <p>About Us</p>
-        <p>FAQ</p>
-        <p>Privacy & Policy</p>
-        <p>Terms & Condition</p>
+        <p>Jl. Gatot Subroto Komplek Taman</p>
+        <p>Tomang Elok Blok C No.123</p>
+        <p>Kota Medan, Sumatera Utara</p>
+        <p>+6282165828164</p>
       </div>
       <div className="footerSupport">
-        <p>
-          <b>Support</b>
-        </p>
-        <p style={{display: "flex", alignItems: "center"}}><FaWhatsapp className="footerIcon"/>+6282165828164</p>
-        <p style={{display: "flex", alignItems: "center"}}><FaRegEnvelope className="footerIcon"/>uptowngownn@gmail.com</p>
-        <p style={{display: "flex", alignItems: "center"}}><FaInstagram className="footerIcon"/>uptown.gown</p>
+        <a href="mailto:uptowngownn@gmail.com" target="_blank" rel="noopener noreferrer"><p><FaRegEnvelope className="footerIcon"/>uptowngownn@gmail.com</p></a>
+        <a href="https://instagram.com/uptown.gown" target="_blank" rel="noopener noreferrer"><p><FaInstagram className="footerIcon"/>uptown.gown</p></a>
+        <a href="https://tiktok.com/@uptown.gown" target="_blank" rel="noopener noreferrer"><p><FaTiktok className="footerIcon"/>uptown.gown</p></a>
       </div>
     </div>
   );
